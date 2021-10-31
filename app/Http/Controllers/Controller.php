9fc138102484007ec25d4b3c6b2dd72ef1a3c5e0
Michal Drozd <michal.drozd@mobilefun.co.uk>
2021-10-31 20:10:49 +0000

    Base controller for listing: index

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * Controller class to return responses with standard resources
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected string $resourceClass;
+    protected string $queryClass;
+    protected string $eloquentClass;
+
+    /**
+     * Base for items listing
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        $resource = new $this->resourceClass(request());
+        $query = new $this->queryClass($this->eloquentClass);
+
+        return  $resource::collection($query->get());
+    }
 }
