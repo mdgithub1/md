8c180d745ad7d62da8be251ec44ca64747af1c7a
Michal Drozd <michal.drozd@mobilefun.co.uk>
2021-10-31 17:53:34 +0000

    Models

---
+++ app/Models/Expenses.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Expenses extends BaseModel
+{
+    /**
+     * @inheritdoc
+     */
+    protected string $table = 'expenses';
+
+    /**
+     * @inheritdoc
+     */
+    protected array $with = [
+        'expenses_type',
+    ];
+
+   /*
+   |----------------------------------------------------------------------
+   | Relationships
+   |----------------------------------------------------------------------
+   */
+
+    /**
+     * Get expense type associated with the image from ImageType
+     */
+    public function expenseType(): belongsTo
+    {
+        return $this->belongsTo(ExpensesType::class, 'expenses_type_id', 'id');
+    }
+}
